2b915027023c898a4d5ac38d49b54dc8f61cea60
nachrstn <christinahartono@student.unud.ac.id>
2024-05-15 18:13:07 +0800

    up seeder mahasiswa

--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -18,22 +18,230 @@ class MahasiswaSeeder extends Seeder
             'nim' => '2105551010',
             'name' => 'Maria Christina Hartono',
             'angkatan' => '2021',
-            'jk' => 'perempuan',
+            'jk' => 'Perempuan',
             'telepon' => '081234567890',
             'agama' => 'Katolik',
             'alamat' => 'Badung, Bali'
         ]);
 
         Mahasiswa::create([
-            'nim' => '2105551011',
-            'name' => 'Maudy Ayunda',
+            'nim' => '2105551017',
+            'name' => 'Ni Putu Meika Dharmayanti',
             'angkatan' => '2021',
-            'jk' => 'perempuan',
+            'jk' => 'Perempuan',
             'telepon' => '081234567812',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551028',
+            'name' => 'Ni Putu Jennifer Putri Ariadi',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '081234567812',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551003',
+            'name' => 'Luh Putu Ary Purwanthi',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '087761551841',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551042',
+            'name' => 'Gusti Ayu Wahyu Whurapsari',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '082247605073',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551005',
+            'name' => 'Kadek Rega Prawira Suyoga',
+            'angkatan' => '2021',
+            'jk' => 'Laki-laki',
+            'telepon' => '089657246596',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551016',
+            'name' => 'Ni Kadek Puja Ari Pertiwi',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '085738052346',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551100',
+            'name' => 'Gusti Ngurah Satya Bagus Partama',
+            'angkatan' => '2021',
+            'jk' => 'Laki-laki',
+            'telepon' => '081246915504',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551002',
+            'name' => 'Benita Carissa Sutrisno Putri',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '085792199021',
+            'agama' => 'Protestan',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551021',
+            'name' => 'Ni Kadek Sinta Nastalia',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '082340600224',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551025',
+            'name' => 'Ni Luh Yunita Ardiani',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '081913914675',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551026',
+            'name' => 'Ni Kadek Intan Diana Putri',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '081218824166',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551083',
+            'name' => 'Ananda Pannadhika Putra',
+            'angkatan' => '2021',
+            'jk' => 'Laki-laki',
+            'telepon' => '082341416947',
+            'agama' => 'Budha',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551106',
+            'name' => 'Moh Prayogi',
+            'angkatan' => '2021',
+            'jk' => 'Laki-laki',
+            'telepon' => '083114748977',
             'agama' => 'Islam',
             'alamat' => 'Badung, Bali'
         ]);
 
+        Mahasiswa::create([
+            'nim' => '2105551033',
+            'name' => 'Bella Melina Pasaribu',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '082252435069',
+            'agama' => 'Protestan',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2005551056',
+            'name' => 'Anak Agung Ngurah Jelantik Tejakusuma',
+            'angkatan' => '2021',
+            'jk' => 'Laki-laki',
+            'telepon' => '628113856546',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551022',
+            'name' => 'Leony Mona Putri Sihaloho',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '083197035484',
+            'agama' => 'Protestan',
+            'alamat' => 'Badung, Bali'
+        ]);
+        Mahasiswa::create([
+            'nim' => '2105551061',
+            'name' => 'Ricardo Dirk Anderson',
+            'angkatan' => '2021',
+            'jk' => 'Laki-laki',
+            'telepon' => '081353609732',
+            'agama' => 'Protestan',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2005551024',
+            'name' => 'I Made Dwipa',
+            'angkatan' => '2021',
+            'jk' => 'Laki-laki',
+            'telepon' => '083119535532',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551119',
+            'name' => 'Made Jiyestha Arturito',
+            'angkatan' => '2021',
+            'jk' => 'Laki-laki',
+            'telepon' => '082144930850',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551064',
+            'name' => 'Kadek Artha Darma Pradnyana',
+            'angkatan' => '2021',
+            'jk' => 'Laki-laki',
+            'telepon' => '08873617442',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2005551125',
+            'name' => 'Ketut Riski Prananda',
+            'angkatan' => '2021',
+            'jk' => 'Laki-laki',
+            'telepon' => '081937025704',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2105551055',
+            'name' => 'Dewa Ayu Maria Ningrum',
+            'angkatan' => '2021',
+            'jk' => 'Perempuan',
+            'telepon' => '082146751425',
+            'agama' => 'Hindu',
+            'alamat' => 'Badung, Bali'
+        ]);
         Mahasiswa::factory(1000)->create();
     }
 }
